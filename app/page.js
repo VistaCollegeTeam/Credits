fb926fb46f910f059d918550e3c004aeace87493
AndyP2000 <152856336+AndyP2000@users.noreply.github.com>
2025-01-23 11:14:25 +0100

    page.js content

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+    return (
+      <div>
+        {/* Navbar */}
+        <nav className="bg-gray-800 p-4">
+          <h1 className="text-white text-center text-2xl font-bold">Credits</h1>
+        </nav>
+  
+        {/* Main Content */}
+        <div className="text-center mt-10">
+          <h2 className="text-3xl font-semibold">Projecten</h2>
+        </div>
+      </div>
+    );
+  }  
